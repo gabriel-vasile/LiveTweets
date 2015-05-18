a1ed11b54fae2c8a092eb686c5f4d04f5aa0b83b
VasileGabriel <bunconta@live.com>
2015-05-19 01:50:23 +0300

    bugfix: no tweets when geoloc disabled

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -39,10 +39,10 @@ tweetInUserArea = function(areaGeo, userGeo) {
 
 createTwit = function() {
     return new TwitMaker({
-        consumer_key: 'FZ7TktT3rQGdFipHVdTRJ29Dk',
-        consumer_secret: '5qkBDtjBQxsBdanNSEZDbM5EPFmoaxKke0V5Qx9lxJvRZbMHFB',
-        access_token: '3111998128-stWXhFNwQLKX4Qjurg7KK7IHVgGhUQ0lBIdJwNp',
-        access_token_secret: 'YlvMOY2O5aSDWvMWGuRMUgialOq1vNCXMpvZHlVDnlzGc'
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: ''
     });
 }
 
